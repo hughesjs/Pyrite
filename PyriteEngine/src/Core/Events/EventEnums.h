a340d07822e83b75a865f7e70a965c4c1991d48d
James Hughes <james@pyrosoftsolutions.co.uk>
2021-09-13 01:26:20 +0100

    Added initial event enums

---
+++ PyriteEngine/src/Core/Events/EventEnums.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../Core.h"
+
+namespace Pyrite
+{
+	enum class EventType
+	{
+		None = 0,
+		AppTick, AppUpdate, AppRender,
+		WindowClose, WindowOpen, WindowResize, WindowFocus, WindowLostFocus, WindowMove,
+		KeyPress, KeyRelease,
+		MouseClickPress, MouseClickRelease, MouseMove, MouseSroll, MouseEnter, MouseLeave
+	};
+
+	enum EventCategory // Has to be a C style enum so we can use bitflags
+	{
+		None = 0,
+		EventCategoryApplication = BIT(1),
+		EventCategoryWindow = BIT(2),
+		EventCategoryKeyboard = BIT(3),
+		EventCategoryMouse = BIT(4),
+	};
+
+	
+}
